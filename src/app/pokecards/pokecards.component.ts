66bea621b5783afb468dcdc06084fec5ea72b7db
Spencer Biri <Cendeu@gmail.com>
2022-02-03 17:22:26 -0600

    Make pokecards per line responsive to width

--- src/app/pokecards/pokecards.component.ts
+++ src/app/pokecards/pokecards.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { UtilitiesService } from '@shared/utilities.service';
-import { PokeModel } from '../shared/pokemodel.model';
 import { PokeserviceService } from '../shared/pokeservice.service';
 
 @Component({
@@ -9,16 +15,33 @@ import { PokeserviceService } from '../shared/pokeservice.service';
   styleUrls: ['./pokecards.component.css'],
 })
 export class PokecardsComponent implements OnInit {
+  screenWidth: number = 0;
+  chunkSize: number = 0;
   pokecards: [] = [];
-
+  unformattedPokecards: [] = [];
   constructor(
     private pokeservice: PokeserviceService,
     private util: UtilitiesService
-  ) {}
+  ) {
+    this.getScreenSize();
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?: any) {
+    this.screenWidth = window.innerWidth;
+    this.chunkSize = Math.floor((this.screenWidth * 0.8) / 240);
+    this.pokecards = this.util.chunkArray(
+      this.unformattedPokecards,
+      this.chunkSize
+    );
+  }
 
   ngOnInit(): void {
     this.pokeservice.updateList.subscribe((list) => {
-      this.pokecards = this.util.chunkArray(list, 5);
+      this.unformattedPokecards = list;
+      this.pokecards = this.util.chunkArray(
+        this.unformattedPokecards,
+        this.chunkSize
+      );
     });
     this.pokeservice.fetchList();
   }
